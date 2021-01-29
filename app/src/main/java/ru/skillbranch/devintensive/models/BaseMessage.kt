8f4391d50be0e8c16bca1270712aef201b055b7c
Bumka <trading2016@yandex.ru>
2021-01-29 20:49:10 +0300

    fix errors for abstractMessage

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -9,16 +9,15 @@ abstract class BaseMessage(
     val isIncoming: Boolean = false,
     val date: Date = Date()
 ) {
-
     abstract fun formatMessage() : String
 
     companion object AbstractFactory{
-        var lastId = -1;
+        var lastId = -1
         fun makeMessage(from: User?, chat: Chat, date:Date=Date(), type:String="text", payload:Any?, isIncoming: Boolean = false) : BaseMessage {
             lastId++
             return when(type){
-                "image"-> ImageMessage("$lastId", from, chat, date=date, image = payload as String)
-                    else -> TextMessage("$lastId", from, chat, date=date, text = payload as String)
+                "image"-> ImageMessage("$lastId", from, chat, date=date, image = payload as String, isIncoming = isIncoming)
+                    else -> TextMessage("$lastId", from, chat, date=date, text = payload as String, isIncoming = isIncoming)
 
             }
         }
